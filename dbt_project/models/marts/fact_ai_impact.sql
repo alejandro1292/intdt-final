f3e3a10aa168e94889d945cdf2ba342992b383d7
Alejandro Ortiz <alejandro.ortiz1292@gmail.com>
2026-03-11 00:57:58 -0300

    Agregamos test de calidad al pipeline

--- dbt_project/models/marts/fact_ai_impact.sql
+++ dbt_project/models/marts/fact_ai_impact.sql
@@ -1,5 +1,14 @@
+{{ config(
+    materialized='incremental',
+    unique_key='job_key || industry_key || location_key || ingested_at',
+    on_schema_change='fail'
+) }}
+
 with stg as (
     select * from {{ ref('stg_ai_impact') }}
+    {% if is_incremental() %}
+    where ingested_at > (select max(ingested_at) from {{ this }})
+    {% endif %}
 )
 
 select
